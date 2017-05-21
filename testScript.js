26b69f6afda7d07e6c874714807aecf1183a20a0
tigerbeetle <jeremiah.4u@gmail.com>
2017-05-21 21:43:52 +0530

    First Upload

---
+++ testScript.js
@@ -0,0 +1,12 @@
+function btOne(){
+	document.getElementById('text').innerHTML="Button One Clicked";
+}
+function btTwo(){
+	document.getElementById('text').innerHTML="Button Two Clicked";
+}
+function btThree(){
+	document.getElementById('text').innerHTML="Button Three Clicked";
+}
+function btFour(){
+	document.getElementById('text').innerHTML="Button Four Clicked";
+}
